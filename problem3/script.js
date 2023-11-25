4b731b6fd329780086d49477422bd70562062586
Redda1242 <d19125768@mytudublin.ie>
2023-11-25 19:41:50 +0000

    start problem 3

---
+++ problem3/script.js
@@ -0,0 +1,42 @@
+const addNoteButton = document.getElementById('addNote');
+const noteInput = document.getElementById('noteInput');
+const notesContainer = document.getElementById('notesContainer');
+
+const addNoteStream = rxjs.fromEvent(addNoteButton, 'click');
+
+addNoteStream.subscribe(() => {
+    const noteText = noteInput.value.trim();
+    if (noteText) {
+        const noteColor = document.getElementById("color-picker").value;
+        createNoteElement(noteText, noteColor);
+        noteInput.value = '';
+    }
+});
+
+function createNoteElement(text, noteColor) {
+    const noteElement = document.createElement('div');
+    noteElement.classList.add('note');
+    noteElement.style.backgroundColor = noteColor;
+
+    const noteTextElement = document.createElement('div');
+    noteTextElement.textContent = text;
+    noteElement.appendChild(noteTextElement);
+
+    const deleteBtn = document.createElement('span');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete');
+    noteElement.appendChild(deleteBtn);
+
+    const editBtn = document.createElement('span');
+    editBtn.textContent = 'Edit';
+    editBtn.classList.add('edit');
+    noteElement.appendChild(editBtn);
+
+    notesContainer.appendChild(noteElement);
+
+    rxjs.fromEvent(deleteBtn, 'click').subscribe(() => noteElement.remove());
+    rxjs.fromEvent(editBtn, 'click').subscribe(() => {
+        const newText = prompt('Edit your note:', noteTextElement.textContent);
+        if (newText !== null) noteTextElement.textContent = newText;
+    });
+}
