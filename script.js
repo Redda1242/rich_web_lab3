673c57b2d0b0fcb729eac33edad65ee57542f2b0
Redda1242 <d19125768@mytudublin.ie>
2023-11-25 18:26:22 +0000

    fix issue with re editing note

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const { fromEvent } = rxjs.umd;
-
 const addNoteButton = document.getElementById('addNote');
 const noteInput = document.getElementById('noteInput');
 const notesContainer = document.getElementById('notesContainer');
 
-const addNoteStream = fromEvent(addNoteButton, 'click');
+const addNoteStream = rxjs.fromEvent(addNoteButton, 'click');
 
 addNoteStream.subscribe(() => {
     const noteText = noteInput.value.trim();
@@ -17,7 +15,10 @@ addNoteStream.subscribe(() => {
 function createNoteElement(text) {
     const noteElement = document.createElement('div');
     noteElement.classList.add('note');
-    noteElement.textContent = text;
+
+    const noteTextElement = document.createElement('div');
+    noteTextElement.textContent = text;
+    noteElement.appendChild(noteTextElement);
 
     const deleteBtn = document.createElement('span');
     deleteBtn.textContent = 'Delete';
@@ -31,9 +32,9 @@ function createNoteElement(text) {
 
     notesContainer.appendChild(noteElement);
 
-    fromEvent(deleteBtn, 'click').subscribe(() => noteElement.remove());
-    fromEvent(editBtn, 'click').subscribe(() => {
-        const newText = prompt('Edit your note:', text);
-        if (newText !== null) noteElement.textContent = newText;
+    rxjs.fromEvent(deleteBtn, 'click').subscribe(() => noteElement.remove());
+    rxjs.fromEvent(editBtn, 'click').subscribe(() => {
+        const newText = prompt('Edit your note:', noteTextElement.textContent);
+        if (newText !== null) noteTextElement.textContent = newText;
     });
 }
